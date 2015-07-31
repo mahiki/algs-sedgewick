47903d65d4f91ce9362fbdc789460f721394e7c4
mahiki <mahiki@users.noreply.github.com>
2015-07-31 09:06:53 -0700

    stuck on reference to nested uf object

--- AlgsI/Percolation.java
+++ AlgsI/Percolation.java
@@ -1,12 +1,9 @@
-import WeightedQuickUnionUF.*;
-
 public class Percolation {
    //  Percolation class: fields, constructor, methods, test client
    //  use WeightedQuickUnionUF class, stdlib.jar
    
    private boolean[][] grid;
    private int N;
-   private boolean topNode, bottomNode;
    
    public Percolation(int N){
       if(N <= 0) throw new IllegalArgumentException("grid size argument N=" + N + "must be > 0");
@@ -14,17 +11,14 @@ public class Percolation {
       grid = new boolean[N+1][N+1];
       grid[0][0] = true;
       grid[N][0] = true;
-      topNode = grid[0][0];  // this is reference to a boolean array value
-      bottomNode = grid[N][0];
 
-      WeightedQuickUnionFindUF uf = new WeightedQuickUnionFindUF(N+1);
+      WeightedQuickUnionUF uf = new WeightedQuickUnionUF(N+1);
       
       for(int i = 1; i <= N; i++){
-         uf.union(grid.convert(0,0), grid.convert(1,i));
-         uf.union(grid.convert(N,0), grid.convert(N,i));
+         uf.union(convert(0,0), convert(1,i));
+         uf.union(convert(N,0), convert(N,i));
       }
    }
-
 
    public void open(int i, int j){
       validate(i);
@@ -32,7 +26,7 @@ public class Percolation {
 
       if(!grid[i][j]){
          grid[i][j] = true;
-         uf.adjacentUnion(i,j);  ///////////////// need to connect all adjacents that are open
+         adjacentUnion(i,j);
       }
    }
 
@@ -43,18 +37,18 @@ public class Percolation {
       return grid[i][j];
    }
    
-   public boolean isFull(int i, int j);{
+   public boolean isFull(int i, int j){
       validate(i);
       validate(j);
       
-      return connected(grid.convert(i,j),grid.convert(0,0));
+      return true;//connected(convert(i,j),convert(0,0));
    }
 
    public boolean percolates(){
-      return connected(grid.convert(N,0),grid.convert(0,0));
+      return true;//  connected(convert(N,0),convert(0,0));
    };
 
-   private void adjacentUnion(int i, int j){  // this unites all valid adjacent grid sites
+   private void adjacentUnion(int i, int j){  // this unites all valid, open adjacent grid sites
    
    }
 
@@ -62,11 +56,11 @@ public class Percolation {
       return (N*(i-1) + j);
    }
    
-   private int[] gridLocation(int k){         // {k}  ->  {i,j} with k:1 to N
+/*   private int[] gridLocation(int k){         // {k}  ->  {i,j} with k:1 to N
       int[] a = {k / N + 1,  ((k - 1) % N) + 1};
       return a;
    } // is this needed?
-   
+*/
 
    private void validate(int p) {
       if (p < 1 || p > N) throw new IndexOutOfBoundsException("index " + p + " is not in [1," + N + "]");
@@ -86,16 +80,15 @@ public class Percolation {
       for(int i = 0; i < perc.grid.length; i++){
          for(int j = 0; j < perc.grid.length; j++) System.out.print("" + perc.grid[i][j] + "\t");
          System.out.println(";");
+      }
          
 // open random sites {1} to {N^2} stdRandom(N,N) if that site is open choose again
 // connect each site
 // test if it percolates
+         
+      StdOut.println("perc.percolates(): " + perc.percolates());
+      perc.union(0,1);
+      
 
-      
-         
-      
-         
-      }
    }
-
 }
