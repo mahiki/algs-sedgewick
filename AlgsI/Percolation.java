c95c45da7868511d25917efdba6766d31e8f2594
mahiki <mahiki@users.noreply.github.com>
2015-08-02 08:03:06 -0700

    remove debuggers, output grid pretty

--- AlgsI/Percolation.java
+++ AlgsI/Percolation.java
@@ -1,3 +1,35 @@
+/****************************************************************************
+ *  Compilation:  javac Percolation.java
+ *  Execution:    java Percolation <integer N>
+ *  Dependencies: StdIn.java StdOut.java WeightedQuickUnionUF.java
+ *
+ *  Percolation test of NxN grid
+ *
+ ****************************************************************************/
+
+/**
+ *  an instance of Percolation will create an NxN grid and a union-find
+ *  array data structure object.  The client can select grid elements and
+ *  open them, which connects to any adjacent open cells.  The client can test
+ *  if there is a connection (implying flow in the physical model) between
+ *  top and bottom rows of the grid.
+ *
+ * API
+ * public class Percolation
+ *              Percolation(int N)     create N-by-N grid, with all sites blocked
+ *         void open(int i, int j)     open site (row i, column j) if not open already
+ *      boolean isOpen(int i, int j)   is site (row i, column j) open?
+ *      boolean isFull(int i, int j)   is site (row i, column j) full?
+ *      boolean percolates()           does the system percolate?
+ *  static void main(String[] args     test client (optional)
+ *
+ *      private methods
+ *  private int convert(int i, int j)  convert grid coordinate to linear component id
+ * private void validate(int p)        tests out-of-bounds
+ * private void adjacentUnion(i,j)     union of {i,j} with all open adjacent cells
+ *
+ *  @author Merlin Robinson
+ */
 public class Percolation {
    
    private boolean[][] grid;
@@ -5,24 +37,19 @@ public class Percolation {
    private WeightedQuickUnionUF uf;
    
    public Percolation(int N){
-      if(N <= 0) throw new IllegalArgumentException("grid size argument N=" + N + " must be > 0");
+      if(N <= 0) throw new IllegalArgumentException(
+            "grid size argument N=" + N + " must be > 0");
       
-      this.N = N;                         // assign the argument to the instance variable N
+      this.N = N;    // this is important :-)
       grid = new boolean[N+1][N+1];
       topNode = 0;
       bottomNode = N*N+1;
-      StdOut.println("bottomNode: " + bottomNode);
-
       uf = new WeightedQuickUnionUF(N*N+2);
-      StdOut.println("uf.count(): " + uf.count());
+
       for(int h = 1; h <= N; h++){
          uf.union(topNode,    convert(1,h));
          uf.union(bottomNode, convert(N,h));
       }
-      StdOut.println("uf.connected(0,1): " + uf.connected(0,1));
-      StdOut.println("uf.connected(N*N,bottomNode): " + uf.connected(N*N,bottomNode));
-      StdOut.println("uf.connected(topNode,bottomNode): " + uf.connected(topNode,bottomNode));
-      StdOut.println("uf.count(): " + uf.count());
    }
 
    public void open(int i, int j){
@@ -59,53 +86,54 @@ public class Percolation {
       if(j<N && grid[i][j+1]) uf.union(convert(i,j+1),convert(i,j));
    }
 
-   private int convert(int i, int j){         // {i,j}  ->  {k} k:1 to N, {i,j > 0}
+   private int convert(int i, int j){  // {i,j}  ->  {k} k:1 to N, {i,j > 0}
       return (N*(i-1) + j);
    }
 
    private void validate(int p) {
-      if (p < 1 || p > N) throw new IndexOutOfBoundsException("index " + p + " is not in [1," + N + "]");
+      if (p < 1 || p > N) throw new IndexOutOfBoundsException(
+            "index " + p + " is not in [1," + N + "]");
    }
 
 
 
-   public static void main(String[] args){   // Test Client
+   public static void main(String[] args){// Test Client
+
       int N = Integer.parseInt(args[0]);
-      
+      double p = 1.0;
+      int openCells = 0;
+      int iRnd = 0;
+      int jRnd = 0;
       Percolation perc = new Percolation(N);
       
-      StdOut.println("contents of perc.grid[][]");
-      for(int i = 1; i < N + 1; i++){
-         for(int j = 1; j < N + 1; j++)
-            StdOut.print("" + perc.grid[i][j] + "{" + perc.convert(i,j) + "}" + "\t");
-         System.out.println("");
-      }
-         
-// open random sites {1} to {N^2} stdRandom(N,N) if that site is open choose again
-// connect each site    StdRandom.uniform(N)  produces [0,N) random integer #s
-// test if it percolates
-         
-      StdOut.println("perc.percolates(): " +  perc.percolates());
-      String str = "X";
       while(!perc.percolates()){
-         int iRnd = StdRandom.uniform(N)+1;
-         int jRnd = StdRandom.uniform(N)+1;
+         iRnd = StdRandom.uniform(N)+1;
+         jRnd = StdRandom.uniform(N)+1;
          if(perc.isOpen(iRnd,jRnd)) continue;
          
          perc.open(iRnd,jRnd);
-         for(int i = 1; i<=N+1; i++) StdOut.print("_");
-         StdOut.println(" iRnd, jRnd: " + iRnd + ", " + jRnd);
-   
-         for(int i = 1; i < N + 1; i++){
-            System.out.print("|");
-            for(int j = 1; j < N + 1; j++){
-               str = perc.grid[i][j] ? " " : "X";
-               StdOut.print("" + str);
-            }
-            System.out.println("|");
-         }
-         for(int i = 1; i<=N+1; i++) StdOut.print("-");
-         StdOut.println("\nperc.percolates(): " +  perc.percolates()+"\n");
+         openCells++;
       }
+
+      String str = "X";
+      StdOut.print("  \t ");
+      for(int i = 1; i<=N; i++) StdOut.print("_");
+      StdOut.println();
+      for(int i = 1; i < N + 1; i++){
+         System.out.print("" + i + "\t|");
+         for(int j = 1; j < N + 1; j++){
+            str = perc.grid[i][j] ? " " : "X";
+            StdOut.print("" + str);
+         }
+            System.out.println("|");
+      }
+      StdOut.print("  \t");
+      for(int i = 1; i<=N+2; i++) StdOut.print("-");
+      
+      p = 1.0 * openCells / (N*N);
+      StdOut.println("\nPercolates: " +  perc.percolates());
+      StdOut.println("Last Opened Cell: {" + iRnd + "," + jRnd + "}");
+      StdOut.println("Open Cells|N^2|Ratio: "
+         + openCells + "|" + N*N + "|" + p + "\n");
    }
 }
---
+++ AlgsI/test-percolation-final.tst
@@ -0,0 +1,52 @@
+> time java Percolation 45 > test-percolation-final.tst
+  	 _____________________________________________
+1	| X       X     XXXXXX XXX   X  X   XX   X X X|
+2	|  X   X   X    XX   X XXX X  X  X XXXX XXX X |
+3	|    X  X   X   XX     XX     XXX X   XX XX XX|
+4	|   X   XX  XXX X  XX X  X   X X       X    X |
+5	|X XXXX XXX   X    X     X  X X XXX XXXX X XXX|
+6	|X     XX    XX XXX XXX XX X X    X    XX     |
+7	|    X   X  X X  X  XXX     X XX    XX    XX  |
+8	|XXX XX  XX  XX  X   X XXXX     XX XX XX  XX X|
+9	|  X X           X  XX  XXX  X XX  X XX XXX X |
+10	|XXX    XX     XXX    X X  X X XX XX   XXXXX  |
+11	|   X X X   X XX XXX  XXXXXX X XX    X  X X X |
+12	|  X X XX   X XXX X X XX     XX X X X  X   X  |
+13	| X   XX   XXXX   X     XX   XX           XXXX|
+14	|XX XXX X     XXXX XXXXXX X     XXX  XXXXX  XX|
+15	|   X X   XX  X XX  XX     XX X  XX  XX  X    |
+16	| XXX    X  XX  X XX   X  X  X XX     X    XXX|
+17	|XX X X  X XX X        XXX  X XXXX    XX X    |
+18	|  X    X  X  X X X X X  XXXX  X X     X      |
+19	|    X X  X      X X X X  X  XX XXXX  X X X  X|
+20	|  X  XX XX XX X   X X X  X  XX X     X X X   |
+21	| X    XXX   XX     X  X X  X  XX   XX X X  X |
+22	| XX     X X X  X X XX X      X   X    X X X  |
+23	|XX XXXX      XX X  X X  XX   X  XXXX       XX|
+24	|XXXX       X     X     XX XX X X    XX   X XX|
+25	|    XX  X XX X   X X X X XXX X    XX    X X  |
+26	|X X    X    X  X  X     X XXX   X XX X  XXX  |
+27	|    X XX   X   XX  X  XX XXXX  X  X X XX  XX |
+28	| X   X   X X X  X           X    XXXXX    XX |
+29	|X    X    XXXX XX X XX  X X  X X X       XXX |
+30	|XXX         X    XX     XX       X X     X  X|
+31	|X   XXX   X XX  XXX   X X X  XX  XX  X XX  X |
+32	|XX XXXX XX  X  X XXX XXXX XXX XX XXXX   X XX |
+33	| X      X  X    XX X      X  X XXX X X XXX XX|
+34	|X X  X X X X X XX X XXXX    XX   X X X X     |
+35	|X    X  XX XXXXX              X X     XX XXXX|
+36	|XXXX X XX X XX  X     X  X XX X  X    XX  XXX|
+37	|XXX   X X  X X XXXX X  XX  X X    X    XXX   |
+38	| XX     XX XX  X  XX X  XXX   X XX   X  X X X|
+39	| X    XX XX   X   XX  X   X XX XXX  XXX X X  |
+40	|XX  XX X   X XX X  X   X    X XXXX  X XX  XX |
+41	|   XXXX X XXX X X    X   X XX    X X XXXX X  |
+42	|   X X  XX X   XXXXX      XX XX      X  X    |
+43	|XXXX  X XXXXX X  X XX  XX     XXX X X XXX    |
+44	|XX   XXX  X  X X X XXXX        XX  X X XXXXX |
+45	| X    XXXXXX    X XX  XXXX       X      XXX X|
+  	-----------------------------------------------
+Percolates: true
+Last Opened Cell: {42,26}
+Open Cells|N^2|Ratio: 1167|2025|0.5762962962962963
+
