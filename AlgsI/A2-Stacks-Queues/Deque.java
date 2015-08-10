5daa3cca62478df7309971459ecea97a8061943f
mahiki <mahiki@users.noreply.github.com>
2015-08-09 23:37:46 -0700

    fill in Deque, add test client class TestDeque

--- AlgsI/A2-Stacks-Queues/Deque.java
+++ AlgsI/A2-Stacks-Queues/Deque.java
@@ -5,8 +5,6 @@
  *
  *  A double-ended queue, "deck".
  ****************************************************************************
- *
- *
  *  API
  *  public class Deque<Item> implements Iterable<Item>
  *               Deque()               construct an empty deque
@@ -20,31 +18,128 @@
  *
  *  @author Merlin Robinson
  */
- 
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class Deque<Item> implements Iterable<Item> {
+   
+   private int N;
+   private Node first, last;
+   
+   public Deque() {
+      first = null;
+      last = first;
+      N = 0;
+   }
 
-   public Deque()                           // construct an empty deque
+   private class Node {
+    Item item;
+    Node next;
+   }
+   
+   public boolean isEmpty() {
+      return first == null;
+   }
+   
+   public int size() {
+      return N;
+   }
+   
+   public void addFirst(Item item) {
+      if(item == null) throw new NullPointerException("pushing null");
+      
+      Node oldfirst = first;
+      first = new Node();
+      first.item = item;
+      first.next = oldfirst;
+      N++;
+   }
+      
+   public void addLast(Item item) {
+      if(item == null) throw new NullPointerException("pushing null");
+      
+      Node oldlast = last;
+      last = new Node();
+      last.item = item;
+      oldlast.next = last;
+      last.next = null;
+      N++;
+      
+   }
 
+   public Item removeFirst() {
+      if (isEmpty()) throw new NoSuchElementException("stack underflow");
+      
+      Item item = first.item;
+      first = first.next;
+      N--;
+      return item;
+   }
+   
+   public Item removeLast() {
+      if (isEmpty()) throw new NoSuchElementException("stack underflow");
+   
+      Item item = last.item;
+      last = null;
+      N--;
+      return item;
+   }
 
-   public boolean isEmpty()                 // is the deque empty?
-   public int size()                        // return the number of items on the deque
-   public void addFirst(Item item)          // add the item to the front
-   public void addLast(Item item)           // add the item to the end
-   public Item removeFirst()                // remove and return the item from the front
-   public Item removeLast()                 // remove and return the item from the end
    public Iterator<Item> iterator()         // return an iterator over items in order from front to end
 
 
-   public static void main(String[] args)   // unit testing
+   public static void main(String[] args) {
+      Deque deck = new Deque();
+    
+      while (!StdIn.isEmpty()) {
+         String s = StdIn.readString();
+      
+         if (s.equals("-")) StdOut.print(stack.pop());
+         else stack.push(s);
+      }
+   }
 }
 
 /*
-Throw a java.lang.NullPointerException if the client attempts to add a null item\nthrow a java.util.NoSuchElementException if the client attempts to remove an item from an empty deque
-throw a java.lang.UnsupportedOperationException if the client calls the remove() method in the iterator
-throw a java.util.NoSuchElementException if the client calls the next() method in the iterator and there are no more items to return.
-
 deque operation in constant worst-case time
 space proportional to the number of items currently in the deque.
-iterato implementation must support each operation (including construction) in constant worst-case time
+iterator implementation must support each operation (including construction) in constant worst-case time
+
+public interface Iterable<Item> {
+ Iterator<Item> iterator();
+}
+
+public interface Iterator<Item> {
+ boolean hasNext();
+ Item next();
+}
+*/
+
+   public Iterator<Item> iterator() { return new ListIterator(); }
+   
+   private class ListIterator implements Iterator<Item> {
+      private Node current = first;
+
+      public boolean hasNext() { return current != null; }
+      public void remove() { throw new UnsupportedOperationException("iterator.remove() not supported");  }
+      
+      public Item next() {
+         if(current.next == null) throw new NoSuchElementException("no element to call next()");
+         Item item = current.item;
+         current = current.next;
+         return item;
+      }
+   }
+
+/*
+UNIT TEST SUGGESTIONS
+if you call addFirst() with the numbers 1 through N in ascending order, then call removeLast() N times, you should see the numbers 1 through N in ascending order.
+
+Arguably even better are randomized unit tests (which we employ heavily in our correctness testing). We recommend that you create a client class with a name like TestDeque, where each unit test is a method in this class. Don't forget to test your iterator.
+
+One very common bug is for something to go wrong when your data structure goes from non-empty to empty and then back to non-empty.
+
+Make sure to test that multiple iterators can be used simultaneously. You can test this with a nested foreach loop. The iterators should operate independently of one another.
 */
  
---
+++ AlgsI/A2-Stacks-Queues/TestDeque.java
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *  Compilation:  javac TestDeque.java
+ *  Execution:    java Deque
+ *  Dependencies: StdIn.java StdOut.java
+ *
+ *  A test client for the Deque class
+ ****************************************************************************
+ *  API
+ *  public class TestDeque
+ *               Deque()               construct an empty deque
+ *       boolean isEmpty()             is the deque empty?
+ *           int size()                return the number of items on the deque
+ *          void addFirst(Item item)   add the item to the front
+ *          void addLast(Item item)    add the item to the end
+ *          Item removeFirst()         remove and return the item from the front
+ *          Item removeLast()          remove and return the item from the end
+ *
+ *  @author Merlin Robinson
+ */
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class TestDeque {
+   
+   
+   
+   
+}
